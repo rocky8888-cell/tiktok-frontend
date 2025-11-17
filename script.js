ad401ee80904371d9bd58d1fcccbf573dc7002f8
yijui0613-glitch <yijui0613@gmail.com>
2025-11-17 10:15:03 +0800

    Update script.js

--- script.js
+++ script.js
@@ -192,3 +192,14 @@ function init(){
 }
 
 document.addEventListener("DOMContentLoaded", init);
+function openFullscreen() {
+    const elem = document.documentElement;
+
+    if (elem.requestFullscreen) {
+        elem.requestFullscreen();
+    } else if (elem.webkitRequestFullscreen) { 
+        elem.webkitRequestFullscreen();
+    } else if (elem.msRequestFullscreen) { 
+        elem.msRequestFullscreen();
+    }
+}
